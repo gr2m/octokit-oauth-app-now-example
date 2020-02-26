a9e1bfe263fef5e795bbcdcc34a9b61e284baabd
Gregor Martynus <gregor@martynus.net>
2020-02-26 10:24:18 -0800

    feat: `{PATCH,DELETE} /api/github/oauth/token`, `DELETE /api/github/oauth/grant`

--- api/github/oauth/grant.ts
+++ api/github/oauth/grant.ts
@@ -1,26 +1,18 @@
 import { NowRequest, NowResponse } from "@now/node";
-import {
-  createToken,
-  checkToken,
-  resetToken,
-  deleteToken
-} from "@octokit/oauth-app";
+import { deleteAuthorization } from "@octokit/oauth-app";
 
 export default async (req: NowRequest, res: NowResponse) => {
+  if (req.method !== "DELETE") {
+    res.status(400);
+    return res.json({
+      error: `${req.method} is not supported`
+    });
+  }
+
   const credentials = {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
   };
-
-  if (req.method === "POST") {
-    const { token, scopes } = await createToken({
-      ...credentials,
-      state: req.body.state as string,
-      code: req.body.code as string
-    });
-
-    return res.json({ token, scopes });
-  }
 
   const token = (req.headers.authorization || "").substr("token ".length);
   if (!token) {
@@ -30,27 +22,13 @@ export default async (req: NowRequest, res: NowResponse) => {
     });
   }
 
-  const method = {
-    GET: checkToken,
-    PATCH: resetToken,
-    DELETE: deleteToken
-  }[req.method];
-
-  if (!method) {
-    res.status(400);
-    return res.json({
-      error: `${req.method} is not supported`
-    });
-  }
-
   try {
-    const result = await method({
+    await deleteAuthorization({
       ...credentials,
       token
     });
 
-    res.status(200);
-    return res.json(result);
+    return res.end();
   } catch (error) {
     if (error.status === 404) {
       res.status(400);
