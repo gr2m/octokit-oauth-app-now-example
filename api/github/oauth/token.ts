4a2c508f97a04121e6546047c7436605371eaab5
Gregor Martynus <gregor@martynus.net>
2020-02-25 21:40:25 -0800

    feat: `GET /api/github/oauth/token`

--- api/github/oauth/token.ts
+++ api/github/oauth/token.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from "@now/node";
-import { createToken } from "@octokit/oauth-app";
+import { createToken, checkToken } from "@octokit/oauth-app";
 
 export default async (req: NowRequest, res: NowResponse) => {
   if (req.method === "POST") {
@@ -11,6 +11,26 @@ export default async (req: NowRequest, res: NowResponse) => {
     });
 
     return res.json({ token, scopes });
+  }
+
+  if (req.method === "GET") {
+    const token = req.headers.authorization?.substr("token ".length);
+
+    if (!token) {
+      res.status(400);
+      return res.json({
+        error: '[@octokit/oauth-app] "Authorization" header is required'
+      });
+    }
+
+    const result = await checkToken({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      token
+    });
+
+    res.status(200);
+    return res.json(result);
   }
 
   throw new Error(`${req.method} not supported`);
